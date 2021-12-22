d45387cab92bfd06a6fc0a5c8095eca9f4542214
sailaja1111 <96243392+sailaja1111@users.noreply.github.com>
2021-12-22 11:12:37 +0530

    Add files via upload

---
+++ addusers.js
@@ -0,0 +1,95 @@
+console.log('This is ES6 version of Project ');
+class Book {
+    constructor(name, author, type) {
+        this.name = name;
+        this.email = email;
+        this.type = type;
+    }
+}
+
+class Display {
+    add(User) {
+        console.log("Adding to UI");
+        let tableBody = document.getElementById('tableBody');
+        let uiString = `<tr>
+                            <td>${book.name}</td>
+                            <td>${book.email}</td>
+                            <td>${book.type}</td>
+                        </tr>`;
+        tableBody.innerHTML += uiString;
+    }
+
+    clear() {
+        let libraryForm = document.getElementById('libraryForm');
+        libraryForm.reset();
+    }
+
+    validate(book) {
+        if (book.name.length < 2 || book.author.length < 2) {
+            return false
+        }
+        else {
+            return true;
+        }
+    }
+
+    show(type, displayMessage) {
+        let message = document.getElementById('message');
+        let boldText;
+        if(type==='success'){
+            boldText = 'Success';
+        }
+        else{
+            boldText = 'Error!';
+        }
+        message.innerHTML = `<div class="alert alert-${type} alert-dismissible fade show" role="alert">
+                                <strong>${boldText}:</strong> ${displayMessage}
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">×</span>
+                                </button>
+                            </div>`;
+        setTimeout(function () {
+            message.innerHTML = ''
+        }, 5000);
+    
+    }
+}
+
+// Add submit event listener to libraryForm
+let libraryForm = document.getElementById('libraryForm');
+libraryForm.addEventListener('submit', libraryFormSubmit);
+
+function libraryFormSubmit(e) {
+    console.log('YOu have submitted library form');
+    let name = document.getElementById('Name').value;
+    let author = document.getElementById('email').value;
+    let type;
+    let fiction = document.getElementById('borrow');
+    let programming = document.getElementById('returns');
+    
+    if (borrow.checked) {
+        type = borrow.value;
+    }
+    else if (returns.checked) {
+        type = returns.value;
+    }
+   
+    }
+
+    let user = new user( Name, Email, Type);
+    console.log(user);
+
+    let display = new Display();
+
+    if (display.validate(user)) {
+
+        display.add(user);
+        display.clear();
+        display.show('success', 'Your book has been successfully added')
+    }
+    else {
+        // Show error to the user
+        display.show('danger', 'Sorry you cannot add this book');
+    }
+
+    e.preventDefault();
---
+++ read.md
@@ -0,0 +1,26 @@
+# NT e-Library
+Library Management System
+The project titled NT e-Library is Library management software for monitoring and controlling the transactions in a library. The project “Simple Library Management System” is developed in HTML,CSS,Javascript, which mainly focuses on basic operations in a library.
+
+Following are modules : 
+
+#User Login :
+
+Login
+Books
+Register
+Take notes
+Notifications
+
+
+#Admin Login : 
+
+Dashboard
+
+Add Books
+Read Books
+List of Admins
+List of Users
+Transcations
+Notifications
+
